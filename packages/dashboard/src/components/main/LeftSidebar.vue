eb9f2fc028a3b9000acb1908d1e2a6f4512874fa
chinatsm <49516465+chinatsm@users.noreply.github.com>
2026-09-18 22:30:49 +0800

    Update LeftSidebar.vue

--- packages/dashboard/src/components/main/LeftSidebar.vue
+++ packages/dashboard/src/components/main/LeftSidebar.vue
@@ -1,41 +1,79 @@
+```vue
 <template>
   <div class="q-pa-md" style="height: 100%">
     <div class="flex column" style="height: 100%">
-      <q-btn v-if="mainStore.apiReadonly" color="red" stack class="q-mb-lg" label="Read only" />
-      <q-btn v-else color="green" icon="add" stack class="q-mb-lg" label="New">
+      <!-- 只读状态 -->
+      <q-btn
+        v-if="mainStore.apiReadonly"
+        color="red"
+        stack
+        class="q-mb-lg"
+        label="只读模式"
+      />
+
+      <!-- 新建菜单 -->
+      <q-btn
+        v-else
+        color="green"
+        icon="add"
+        stack
+        class="q-mb-lg"
+        label="新建"
+      >
         <q-menu>
           <q-list>
-            <q-item clickable v-close-popup @click="$refs.createFile.open()">
+            <!-- 新建文件 -->
+            <q-item
+              clickable
+              v-close-popup
+              @click="$refs.createFile.open()"
+            >
               <q-item-section>
                 <q-item-label>
                   <q-icon name="note_add" size="sm" />
-                  New File
+                  新建文件
                 </q-item-label>
               </q-item-section>
             </q-item>
-            <q-item clickable v-close-popup @click="$refs.createFolder.open()">
+
+            <!-- 新建文件夹 -->
+            <q-item
+              clickable
+              v-close-popup
+              @click="$refs.createFolder.open()"
+            >
               <q-item-section>
                 <q-item-label>
                   <q-icon name="create_new_folder" size="sm" />
-                  New Folder
+                  新建文件夹
                 </q-item-label>
               </q-item-section>
             </q-item>
 
-            <q-item clickable v-close-popup @click="$bus.emit('openFilesUploader')">
+            <!-- 上传文件 -->
+            <q-item
+              clickable
+              v-close-popup
+              @click="$bus.emit('openFilesUploader')"
+            >
               <q-item-section>
                 <q-item-label>
                   <q-icon name="upload_file" size="sm" />
-                  Upload Files
+                  上传文件
                 </q-item-label>
               </q-item-section>
             </q-item>
 
-            <q-item clickable v-close-popup @click="$bus.emit('openFoldersUploader')">
+            <!-- 上传文件夹 -->
+            <q-item
+              clickable
+              v-close-popup
+              @click="$bus.emit('openFoldersUploader')"
+            >
               <q-item-section>
                 <q-item-label>
                   <q-icon name="folder" size="sm" />
-                  Upload Folders
+                  上传文件夹
                 </q-item-label>
               </q-item-section>
             </q-item>
@@ -43,42 +81,106 @@
         </q-menu>
       </q-btn>
 
-      <q-btn class="q-mb-sm" @click="gotoFiles" color="blue" icon="folder_copy" label="Files" stack />
-      <q-btn v-if="mainStore.config && mainStore.config.emailRouting !== false" class="q-mb-sm" @click="gotoEmail" color="blue" icon="email" label="Email" stack />
+      <!-- 文件 -->
+      <q-btn
+        class="q-mb-sm"
+        @click="gotoFiles"
+        color="blue"
+        icon="folder_copy"
+        label="文件"
+        stack
+      />
 
-      <q-btn class="q-mb-sm q-mt-auto q-mb-0" @click="infoPopup=true" color="secondary" icon="question_mark"
-             label="Info"
-             stack />
+      <!-- 邮件 -->
+      <q-btn
+        v-if="mainStore.config && mainStore.config.emailRouting !== false"
+        class="q-mb-sm"
+        @click="gotoEmail"
+        color="blue"
+        icon="email"
+        label="邮件"
+        stack
+      />
+
+      <!-- 关于 / 信息 -->
+      <q-btn
+        class="q-mb-sm q-mt-auto q-mb-0"
+        @click="infoPopup=true"
+        color="secondary"
+        icon="question_mark"
+        label="关于"
+        stack
+      />
     </div>
   </div>
 
+  <!-- 关于信息弹窗 -->
   <q-dialog v-model="infoPopup" persistent no-route-dismiss>
     <q-card>
       <q-card-section>
-        <div class="text-h6">🎉 Thank you for using R2-Explorer! 🚀</div>
+        <div class="text-h6">
+          🎉 感谢使用 R2-Explorer！🚀
+        </div>
       </q-card-section>
 
       <q-card-section class="q-pt-none">
-        You are running version <b>{{ mainStore.version }}</b><br>
-        <template v-if="updateAvailable">
-          Latest version is <b>{{latestVersion}}</b>, learn how to <a href="https://r2explorer.com/getting-started/updating-your-project/" target="_blank">update your instance here</a>.<br>
-        </template>
+        当前运行版本：
+        <b>{{ mainStore.version }}</b>
         <br>
+
+        <!-- 有新版本 -->
+        <template v-if="updateAvailable">
+          最新版本：
+          <b>{{ latestVersion }}</b>
+          <br>
+
+          查看
+          <a
+            href="https://r2explorer.com/getting-started/updating-your-project/"
+            target="_blank"
+          >
+            如何更新您的实例
+          </a>
+          。
+          <br>
+        </template>
+
+        <br>
+
+        <!-- 已启用身份验证 -->
         <template v-if="mainStore.auth">
-          <b>Authentication</b><br>
-          Method: {{ mainStore.auth.type }}<br>
-          Username: {{ mainStore.auth.username }}
+          <b>身份验证</b>
+          <br>
+
+          验证方式：
+          {{ mainStore.auth.type }}
+          <br>
+
+          用户名：
+          {{ mainStore.auth.username }}
         </template>
+
+        <!-- 未启用身份验证 -->
         <template v-else>
-          Not authenticated
+          未启用身份验证
         </template>
+
         <br><br>
-        <b>Server Configuration</b><br>
+
+        <!-- 服务器配置 -->
+        <b>服务器配置</b>
+        <br>
+
         {{ JSON.stringify(mainStore.config, null, 2) }}
       </q-card-section>
 
       <q-card-actions align="right">
-        <q-btn flat label="OK" color="primary" v-close-popup />
+        <q-btn
+          flat
+          label="确定"
+          color="primary"
+          v-close-popup
+        />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -94,84 +196,60 @@ import { useMainStore } from "stores/main-store";
 import { defineComponent } from "vue";
 
 export default defineComponent({
-	name: "LeftSidebar",
-	data: () => ({
-		infoPopup: false,
-		updateAvailable: false,
-		latestVersion: "",
-	}),
-	components: { CreateFolder, CreateFile },
-	methods: {
-		gotoEmail: function () {
-			if (this.selectedApp !== "email") this.changeApp("email");
-		},
-		gotoFiles: function () {
-			if (this.selectedApp !== "files") this.changeApp("files");
-		},
-		changeApp: function (app) {
-			this.$router.push({
-				name: `${app}-home`,
-				params: { bucket: this.selectedBucket },
-			});
-		},
-		isUpdateAvailable: (currentVersion, latestVersion) => {
-			// Split versions into parts and convert to numbers
-			const current = currentVersion.split(".").map(Number);
-			const latest = latestVersion.split(".").map(Number);
+  name: "LeftSidebar",
 
-			// Compare major version
-			if (latest[0] > current[0]) return true;
-			if (latest[0] < current[0]) return false;
+  data: () => ({
+    infoPopup: false,
+    updateAvailable: false,
+    latestVersion: "",
+  }),
 
-			// Compare minor version
-			if (latest[1] > current[1]) return true;
-			if (latest[1] < current[1]) return false;
+  components: {
+    CreateFolder,
+    CreateFile,
+  },
 
-			// Compare patch version
-			if (latest[2] > current[2]) return true;
+  methods: {
+    gotoEmail: function () {
+      if (this.selectedApp !== "email") this.changeApp("email");
+    },
 
-			return false;
-		},
-	},
-	computed: {
-		selectedBucket: function () {
-			return this.$route.params.bucket;
-		},
-		selectedApp: function () {
-			return this.$route.name?.split("-")[0] || "files";
-		},
-	},
-	async mounted() {
-		const resp = await fetch(
-			"https://api.github.com/repos/G4brym/R2-Explorer/releases/latest",
-		);
-		if (!resp.ok) {
-			console.log("Unable to retrieve latest r2-explorer updates :(");
-			console.log(
-				"Manually check them here: https://github.com/G4brym/R2-Explorer/releases",
-			);
-		} else {
-			const parsed = await resp.json();
-			const latestVersion = parsed.tag_name.replace("v", "");
-			if (this.isUpdateAvailable(this.mainStore.version, latestVersion)) {
-				this.latestVersion = latestVersion;
-				this.updateAvailable = true;
-			}
-		}
-	},
-	setup() {
-		const mainStore = useMainStore();
+    gotoFiles: function () {
+      if (this.selectedApp !== "files") this.changeApp("files");
+    },
 
-		return {
-			mainStore,
-		};
-	},
-});
-</script>
+    changeApp: function (app) {
+      this.$router.push({
+        name: `${app}-home`,
+        params: { bucket: this.selectedBucket },
+      });
+    },
 
-<style scoped>
-.q-btn {
-  max-width: 100%;
-  padding: 4px;
-}
-</style>
+    isUpdateAvailable: (currentVersion, latestVersion) => {
+      // Split versions into parts and convert to numbers
+      const current = currentVersion.split(".").map(Number);
+      const latest = latestVersion.split(".").map(Number);
+
+      // Compare major version
+      if (latest[0] > current[0]) return true;
+      if (latest[0] < current[0]) return false;
+
+      // Compare minor version
+      if (latest[1] > current[1]) return true;
+      if (latest[1] < current[1]) return false;
+
+      // Compare patch version
+      if (latest[2] > current[2]) return true;
+
+      return false;
+    },
+  },
+
+  computed: {
+    selectedBucket: function () {
+      return this.$route.params.bucket;
+    },
+
+    selectedApp: function () {
+      return this.$route.name?.split("-")[0] ||
+```
